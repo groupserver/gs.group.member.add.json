761791e02e9c7bd2afc3c3a8f8b4253242cbd42f
Michael JasonSmith <mpj17@onlinegroups.net>
2012-11-07 13:29:54 +1100

    Code cleanup, with the help of Ninja IDE

--- gs/group/member/add/add.py
+++ gs/group/member/add/add.py
@@ -5,7 +5,6 @@ from zope.formlib import form
 from five.formlib.formbase import PageForm
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.CustomUserFolder.userinfo import userInfo_to_anchor
-from Products.XWFCore.XWFUtils import get_the_actual_instance_from_zope
 from Products.GSGroup.groupInfo import groupInfo_to_anchor
 from Products.GSGroupMember.groupmembership import user_member_of_group
 from Products.GSProfile.edit_profile import select_widget, wym_editor_widget
@@ -18,6 +17,7 @@ from gs.group.member.invite.base.utils import set_digest
 from audit import Auditor, ADD_NEW_USER, ADD_OLD_USER, ADD_EXISTING_MEMBER
 from addfields import AddFields
 from gs.group.member.join.interfaces import IGSJoiningUser
+
 
 class AddEditProfileForm(PageForm):
     label = u'Add a New Group Member, Without Verification'
@@ -35,7 +35,7 @@ class AddEditProfileForm(PageForm):
 
     @property
     def form_fields(self):
-        if self.__formFields == None:
+        if self.__formFields is None:
             self.__formFields = form.Fields(self.addFields.adminInterface,
                 render_context=False)
             tz = self.__formFields['tz']
@@ -43,8 +43,8 @@ class AddEditProfileForm(PageForm):
             self.__formFields['biography'].custom_widget = wym_editor_widget
             self.__formFields['delivery'].custom_widget = radio_widget
         return self.__formFields
-        
-    def setUpWidgets(self, ignore_request=False):        
+
+    def setUpWidgets(self, ignore_request=False):
         data = {}
 
         siteTz = self.siteInfo.get_property('tz', 'UTC')
@@ -55,11 +55,11 @@ class AddEditProfileForm(PageForm):
             self.form_fields, self.prefix, self.context,
             self.request, form=self, data=data,
             ignore_request=ignore_request)
-        
-    @form.action(label=u'Add Without Verification', failure='handle_add_action_failure')
+
+    @form.action(label=u'Add', failure='handle_add_action_failure')
     def handle_add(self, action, data):
         self.actual_handle_add(action, data)
-        
+
     def handle_add_action_failure(self, action, data, errors):
         if len(errors) == 1:
             self.status = u'<p>There is an error:</p>'
@@ -69,53 +69,53 @@ class AddEditProfileForm(PageForm):
     # Non-Standard methods below this point
     @property
     def groupInfo(self):
-        if self.__groupInfo == None:
+        if self.__groupInfo is None:
             self.__groupInfo = \
                 createObject('groupserver.GroupInfo', self.context)
         return self.__groupInfo
-        
+
     @property
     def adminInfo(self):
-        if self.__adminInfo == None:
+        if self.__adminInfo is None:
             self.__adminInfo = createObject('groupserver.LoggedInUser',
                 self.context)
         return self.__adminInfo
-    
+
     @property
     def adminWidgets(self):
         return self.addFields.get_admin_widgets(self.widgets)
 
     @property
     def profileWidgets(self):
-        return self.addFields.get_profile_widgets(self.widgets)    
-    
-    @property  
+        return self.addFields.get_profile_widgets(self.widgets)
+
+    @property
     def requiredProfileWidgets(self):
         widgets = self.addFields.get_profile_widgets(self.widgets)
-        widgets = [widget for widget in widgets if widget.required == True]
-        
+        widgets = [widget for widget in widgets if widget.required]
+
         return widgets
-    
+
     @property
     def optionalProfileWidgets(self):
         widgets = self.addFields.get_profile_widgets(self.widgets)
-        widgets = [widget for widget in widgets if widget.required == False]
-            
+        widgets = [widget for widget in widgets if not(widget.required)]
+
         return widgets
-        
+
     def actual_handle_add(self, action, data):
         userInfo = None
-        
+
         acl_users = self.context.acl_users
         toAddr = data['toAddr'].strip()
-        
+
         emailChecker = NewEmailAddress(title=u'Email')
         emailChecker.context = self.context
         e = u'<code class="email">%s</code>' % toAddr
         g = groupInfo_to_anchor(self.groupInfo)
         try:
             emailChecker.validate(toAddr)
-        except EmailAddressExists, errorVal:
+        except EmailAddressExists:
             user = acl_users.get_userByEmail(toAddr)
             assert user, 'User for address <%s> not found' % toAddr
             # get the user object in the context of the group and site
@@ -126,7 +126,7 @@ class AddEditProfileForm(PageForm):
             u = userInfo_to_anchor(userInfo)
             if user_member_of_group(user, self.groupInfo):
                 auditor.info(ADD_EXISTING_MEMBER, toAddr)
-                self.status = u'''<li>The person with the email address %s 
+                self.status = u'''<li>The person with the email address %s
 &#8213; %s &#8213; is already a member of %s.</li>''' % (e, u, g)
                 self.status = u'%s<li>No changes to the profile of '\
                   '%s have been made.</li>' % (self.status, u)
@@ -147,7 +147,7 @@ class AddEditProfileForm(PageForm):
                                self.context, toAddr)
             evu.add_verification_id(vid)
             evu.verify_email(vid)
-            
+
             # get the user object in the context of the group and site
             userInfo = createObject('groupserver.UserFromId',
                                   self.groupInfo.groupObj,
@@ -156,7 +156,7 @@ class AddEditProfileForm(PageForm):
             auditor = self.get_auditor(userInfo)
             auditor.info(ADD_NEW_USER, toAddr)
             joininguser = IGSJoiningUser(userInfo)
-            joininguser.join(self.groupInfo)    
+            joininguser.join(self.groupInfo)
             self.set_delivery(userInfo, data['delivery'])
             u = userInfo_to_anchor(userInfo)
             self.status = u'''<li>A profile for %s has been created, and
@@ -172,16 +172,14 @@ given the email address %s.</li>\n''' % (u, e)
         fields = self.form_fields.select(*self.addFields.profileFieldIds)
         for field in fields:
             field.interface = self.addFields.profileInterface
-            
-        changed = form.applyChanges(userInfo.user, fields, data)
+
+        form.applyChanges(userInfo.user, fields, data)
         set_digest(userInfo, self.groupInfo, data)
 
     def get_auditor(self, userInfo):
-        ctx = get_the_actual_instance_from_zope(self.context)
-        
         auditor = Auditor(self.siteInfo, self.groupInfo,
                     self.adminInfo, userInfo)
-        
+
         return auditor
 
     def set_delivery(self, userInfo, delivery):
@@ -192,4 +190,3 @@ given the email address %s.</li>\n''' % (u, e)
             userInfo.user.set_enableDigestByKey(self.groupInfo.id)
         elif delivery == 'web':
             userInfo.user.set_disableDeliveryByKey(self.groupInfo.id)
-
